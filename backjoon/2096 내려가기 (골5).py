db44cbc430c1e44325a747827e440b0adbd5cdbb
배민수 <duecost@gmail.com>
2025-02-24 16:14:38 +0900

    주석 추가

--- backjoon/2096 내려가기 (골5).py
+++ backjoon/2096 내려가기 (골5).py
@@ -5,6 +5,13 @@ N = int(input().rstrip())
 score_min = [[0] * 3 for _ in range(2)]
 score_max = [[0] * 3 for _ in range(2)]
 
+# DP 풀이
+# 어떤 행 row에 대해, 0열로 도착한다고 가정할 때 최대값은
+# row-1 행의 0열과 1열 중 최대값에다 row행의 0열 값을 더한 값이다.
+# row행의 1열은 row-1행의 0열, 1열, 2열을 대상으로 위 논리를 수행한다.
+# row행의 2열은 row-1행의 1열, 2열을 대상으로 위 논리를 수행한다.
+# 이전 행과 다음 행의 정보만 표현하면 충분하므로, 메모리 최적화를 위해
+# DP 리스트는 두 개의 행으로 구성했다.
 for _ in range(N):
     nums = [*map(int, input().rstrip().split())]
 
